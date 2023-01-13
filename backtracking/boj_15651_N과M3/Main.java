4335cc688cc8629ead71086f538ee988db97e64f
kangCoder <khsofficial1213@gmail.com>
2023-01-13 16:14:09 +0900

    [backtracking] 15651 N과 M 3 | N과 M 시리즈, 중복 허용 수열 만들기. BufferedWriter 안쓰니까 시간초과 났음

---
+++ backtracking/boj_15651_N과M3/Main.java
@@ -0,0 +1,43 @@
+package backtracking.boj_15651_N과M3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedWriter bw;
+    public static int N, M;
+    public static int[] output;
+
+    public static void backTracking(int length) throws IOException {
+        if (length == M) {
+            for (int i : output) {
+                bw.write(i + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            output[length] = i;
+            backTracking(length + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        output = new int[M];
+        backTracking(0);
+        bw.flush();
+    }
+
+}
