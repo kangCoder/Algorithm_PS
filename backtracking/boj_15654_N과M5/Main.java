31bb3f45bd73ec5cbc946993a33c3b6bc2b153e5
kangCoder <khsofficial1213@gmail.com>
2023-01-15 02:54:41 +0900

    [backtracking] 15654 N과 M 5 | N과 M 시리즈.

---
+++ backtracking/boj_15654_N과M5/Main.java
@@ -0,0 +1,60 @@
+package backtracking.boj_15654_N과M5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+    public static int N, M;
+    public static int[] input, output;
+    public static boolean[] isUsed;
+
+    public static void backTracking(int length) throws IOException {
+        if (length == M) {
+            for (int i : output) {
+                bw.write(i + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            if (!isUsed[i]) {
+                output[length] = input[i];
+                isUsed[i] = true;
+                backTracking(length + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        input = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+
+        output = new int[M];
+        isUsed = new boolean[N];
+        backTracking(0);
+
+        bw.flush();
+    }
+
+}
