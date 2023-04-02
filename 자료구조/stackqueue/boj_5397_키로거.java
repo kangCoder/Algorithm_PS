6e6ac20bca522d624019f26282121112cf08ff86
kangCoder <khsofficial1213@gmail.com>
2023-04-03 03:45:53 +0900

    [stack] 5397 키로거 | 그냥 ArrayList를 쓰면 시간 초과. 2개의 Stack으로 O(n)으로 구현해야 함. (연결리스트 쓰는 방법도 나중에 풀어보기

--- 자료구조/stackqueue/boj_5397_키로거.java
+++ 자료구조/stackqueue/boj_5397_키로거.java
@@ -19,6 +19,10 @@ public class boj_5397_키로거 {
             Stack<Character> postStack = new Stack<>();
 
             char[] line = br.readLine().toCharArray();
+            //cursor를 기준으로 왼쪽은 prevStack, 오른쪽은 postStack에 넣게 만든다.
+            //<가 나오면 cursor가 왼쪽으로 움직이니 prev에서 하나 꺼내서 post에 넣어준다.
+            //다 끝났으면 prev에 남아있는걸 post에 다 옮기고 post를 출력하면 된다.
+            //O(n)에 끝내야 함.
             for (char c : line) {
                 switch (c) {
                     case '<':
