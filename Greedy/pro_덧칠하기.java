ad00c7aa8d29d038bc03fcf22e0738a424adede0
kangCoder <khsofficial1213@gmail.com>
2023-04-24 00:33:07 +0900

    [greedy] programmers 덧칠하기 | 단순하게 생각하기

---
+++ Greedy/pro_덧칠하기.java
@@ -0,0 +1,21 @@
+package greedy;
+
+public class pro_덧칠하기 {
+
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+
+        int max = 0;
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] < max) {
+                continue;
+            }
+
+            answer++;
+            max = section[i] + m;
+        }
+
+        return answer;
+    }
+
+}
