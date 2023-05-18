844ff0702ea377e3b8f51ff1d159dc48131efb7f
kangCoder <khsofficial1213@gmail.com>
2023-05-18 17:28:46 +0900

    [dp] boj 11659 구간 합 구하기 4 | DP

---
+++ dp/boj_11659_구간합구하기4.java
@@ -0,0 +1,39 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj_11659_구간합구하기4 {
+
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int tc = 0; tc < M; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            bw.write(arr[j] - arr[i - 1] + "\n");
+        }
+
+        bw.flush();
+    }
+}
