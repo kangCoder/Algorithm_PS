ab494f017d3d7ab9529823206a886d84f4559c75
kangCoder <khsofficial1213@gmail.com>
2023-06-10 03:08:07 +0900

    [구현] programmers 테이블 해시 함수 | bitwise 연산

--- simulation/pro_테이블해시함수.java
+++ simulation/pro_테이블해시함수.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class pro_테이블해시함수 {
 
     public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+
         //col번째 열을 기준으로 오름차순 정렬
         //값이 동일하면 첫 번째 값 비교로 내림차순
         Arrays.sort(data, (a, b) -> {
@@ -16,23 +18,12 @@ public class pro_테이블해시함수 {
         });
 
         //i번째 행의 tuple의 각 컬럼의 값을 i로 나눈 나머지의 합 S_i
-        int[] S = new int[row_end - row_begin + 1];
         for (int i = row_begin - 1; i <= row_end - 1; i++) {
+            int S_i = 0;
             for (int j = 0; j < data[i].length; j++) {
-                //System.out.println(data[i][j] + " mod " + (i + 1));
-                S[i - (row_begin - 1)] += data[i][j] % (i + 1);
+                S_i += data[i][j] % (i + 1);
             }
-            //System.out.println("S=" + S[i - (row_begin - 1)]);
-        }
-
-        //S_i를 bitwise XOR
-        if (S.length == 1) {
-            return S[0];
-        }
-
-        int answer = S[0];
-        for (int i = 1; i < S.length; i++) {
-            answer ^= S[i];
+            answer ^= S_i;
         }
 
         return answer;
