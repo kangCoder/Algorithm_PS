5cd23b3c6383f776e9d4938e697bd31932167595
kangCoder <khsofficial1213@gmail.com>
2023-09-28 19:33:25 +0900

    [백트래킹] boj 2404 단위 분수로 분할 | 매개변수 설정이 어려움

---
+++ backtracking/boj_2404_단위분수로분할.java
@@ -0,0 +1,41 @@
+package backtracking;
+
+import java.util.*;
+import java.io.*;
+
+public class boj_2404_단위분수로분할 {
+
+    static int p, q, a, n, ans = 0;
+
+    static void backTracking(int nume, int deno, int start, int cnt) {
+        if (deno * p == nume * q) {
+            ans++;
+            return;
+        }
+
+        if (cnt == n) {
+            return;
+        }
+
+        for (int i = start; i * deno <= a; i++) {
+            int nxtNume = deno + nume * i;
+            int nxtDeno = deno * i;
+            backTracking(nxtNume, nxtDeno, i, cnt + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        p = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        backTracking(0, 1, 1, 0);
+
+        System.out.println(ans);
+    }
+
+}
